db14b330a3bba232315642178c974225433777ee
Gustavo Amaro <gustavoamarommiv@gmail.com>
2022-07-18 21:28:47 -0300

    Data view

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,8 @@ function App() {
   const [publishedIn, setPublishedIn] = useState(0);
   const [rating, setRating] = useState(0);
 
+  const [readBooks, setReadBooks] = useState([]);
+
   const addBookRead = () => {
     Axios.post("http://localhost:3000/registerRead", {
       title: title, 
@@ -19,10 +21,22 @@ function App() {
       console.log('success');
     });
   }
+
+  const getReadBooks = () => {
+    Axios.get("http://localhost:3000/readBooks").then((response) => {
+      setReadBooks(response.data);
+    });
+  }
  
   return (
     <div className="App">
+      <div className="Title">
+        <h1>Library</h1>
+      </div>
+
       <div className="register">
+        <h2>Register your books</h2>
+
         <label>Title </label>
         <input type="text" onChange={(event) => (setTitle(event.target.value))} /> 
 
@@ -35,7 +49,22 @@ function App() {
         <label>Rating </label>
         <input type="number" onChange={(event) => (setRating(event.target.value))} />
 
-        <button type="submit" onClick={addBookRead}> Sign In </button>
+        <button type="submit" onClick={addBookRead}> REGISTER </button>
+      </div>
+      
+      <div className="readBooks">
+        <h2>Read books list</h2>
+
+        <button onClick={getReadBooks} className="readBtn">SHOW BOOKS</button>
+
+        {readBooks.map((val, key) => {
+          return <div className="readBook"> 
+            <h3>Title: {val.title}</h3>
+            <h3>Author: {val.author}</h3>
+            <h3>Published In: {val.publishedIn}</h3>
+            <h3>Rating: {val.rating}</h3>
+          </div>;
+        })}
       </div>
       
     </div>
