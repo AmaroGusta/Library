b3cc88e092489536586b7403092964b0ba4b6e23
Gustavo Amaro <69257054+AmaroGusta@users.noreply.github.com>
2022-07-21 19:40:06 -0300

    Non problematic names to the columns

--- server/books.sql
+++ server/books.sql
@@ -1,10 +1,10 @@
 create database if not exists books;
 use books;
 
-create table read (
+create table readAlready (
     id int not null primary key auto_increment,
     title varchar(150) not null,
     author varchar (150) not null, 
-    year int,
+    publishedIn int,
     rating int
 );
